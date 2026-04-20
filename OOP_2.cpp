5299216c029266e5bc4093b4676fdefe370a9609
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-20 11:42:43 +0700

    membuat program kelas barang

---
+++ OOP_2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class barang {
+
+public:
+    string nama;
+    int jumlah;
+    string kategori;
+    int tanggalproduksi;
+
+    void printdata() {
+        cout << "Nama: " << nama << endl;
+        cout << "Jumlah: " << jumlah << endl;
+        cout << "Kategori: " << kategori << endl;
+        cout << "Tanggal Produksi: " << tanggalproduksi << endl;
+    }
+}; //batas class
+
+int main(){
+
+    barang brg;
+
+    barang elektronik;
+    barang non_elektronik;
+
+    elektronik.nama = "Laptop";
+    elektronik.jumlah = 1;
+    elektronik.kategori = "Elektronik";
+    elektronik.tanggalproduksi = 20230101;
+
+    non_elektronik.nama = "Meja";
+    non_elektronik.jumlah = 1;
+    non_elektronik.kategori = "Non-Elektronik";
+    non_elektronik.tanggalproduksi = 20230201;
+
+    elektronik.printdata();
+    non_elektronik.printdata();
+    brg.printdata();
+}
